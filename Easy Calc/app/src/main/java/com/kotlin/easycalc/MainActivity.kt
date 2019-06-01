7c5178c34d125e5f421f991ca27fb5d3c071f2e5
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2019-05-31 21:28:25 -0300

    Now, buttons write on screen

--- Easy Calc/app/src/main/java/com/kotlin/easycalc/MainActivity.kt
+++ Easy Calc/app/src/main/java/com/kotlin/easycalc/MainActivity.kt
@@ -2,12 +2,50 @@ package com.kotlin.easycalc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var strings: ArrayList<String> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView.text = ""
+        strings.add("")
+    }
+
+    fun onClickEqual(view: View) {
+
+    }
+
+    fun onClickNumber(view: View) {
+        val btn: Button? = view as? Button
+//        val strings = textView.text.split(" ")
+
+//        if (btn?.text == "." && !strings[strings.lastIndex].contains('.') || btn?.text != ".")
+//            textView.text = "${textView.text}${btn?.text}"
+    }
+
+    fun onClickOperation(view: View) {
+        val btn: Button? = view as? Button
+
+        textView.text = "${textView.text}${btn?.text}"
+    }
+
+    fun onClickDelete(view: View) {
+        textView.text = remLastChar(textView.text.toString())
+        if (textView.text.isNotEmpty() && textView.text.last() == ' ')
+            textView.text = remLastChar(textView.text.toString())
+    }
+
+    private fun remLastChar(str: String) : String {
+        when(str.isNotEmpty()) {
+            true -> return str.substring(0, str.lastIndex)
+            false -> return ""
+        }
     }
 }
